02a50d59e07e10659c9d437d82171854c11e7b99
Luan <luanggcontato@gmail.com>
2020-12-03 15:21:16 -0300

    Added Banks on execution list

---
+++ src/importacaoExcelPadrao/EcofetalImportation.java
@@ -0,0 +1,124 @@
+package importacaoExcelPadrao;
+
+import Entity.Executavel;
+import Robo.AppRobo;
+import TemplateContabil.Control.ControleTemplates;
+import TemplateContabil.Model.Entity.Importation;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class EcofetalImportation {
+
+    private static String nomeApp = "";
+    private static final String pastaEmpresa = "Ecofetal  Serviço de Auxílio Diagnóstico e Terapia Ltda";
+    private static final String pastaAnual = "Movimentos";
+    private static final String pastaMensal = "Bancos";
+    private static int mes;
+    private static int ano;
+
+    public static void main(String[] args) {
+        try {
+            AppRobo robo = new AppRobo(nomeApp);
+
+            robo.definirParametros();
+
+            /**
+             * ----------------------- COLUNAS --------------------*
+             */
+            Map<String, String> colunasCaixaFixo = new HashMap<>();
+            colunasCaixaFixo.put("data", "A");
+            colunasCaixaFixo.put("pretexto", "B");
+            colunasCaixaFixo.put("historico", "F");
+            colunasCaixaFixo.put("entrada", "G");
+            colunasCaixaFixo.put("saida", "-H");
+
+            Map<String, String> colunasBBMovimento = new HashMap<>();
+            colunasBBMovimento.put("data", "B");
+            colunasBBMovimento.put("historico", "I");
+            colunasBBMovimento.put("valor", "G");           
+        
+            Map<String, String> colunasBBConvenio = new HashMap<>();
+            colunasBBConvenio.put("data", "B");
+            colunasBBConvenio.put("historico", "I;C");
+            colunasBBConvenio.put("valor", "E");            
+
+            Map<String, String> colunasBradesco = new HashMap<>();
+            colunasBradesco.put("data", "B");
+            colunasBradesco.put("historico", "I;C");
+            colunasBradesco.put("valor", "F");
+            
+            Map<String, String> colunasCEF = new HashMap<>();
+            colunasCEF.put("data", "B");
+            colunasCEF.put("historico", "I;C");
+            colunasCEF.put("valor", "F");
+            
+            Map<String, String> colunasServicoop = new HashMap<>();
+            colunasServicoop.put("data", "B");
+            colunasServicoop.put("historico", "J;C");
+            colunasServicoop.put("valor", "F");
+            
+            Map<String, String> colunasSantander = new HashMap<>();
+            colunasSantander.put("data", "B");
+            colunasSantander.put("historico", "C");
+            colunasSantander.put("valor", "F");
+
+            /**
+             * ----------------------- TERMINA COLUNAS --------------------*
+             */
+            mes = robo.getParametro("mes").getMes();
+            ano = robo.getParametro("ano").getInteger();
+            nomeApp = "Importação Bancos " + pastaEmpresa;
+
+            robo.setNome(nomeApp);
+            robo.executar(
+                    templateBanco(5, "Caixa Fixo", "CAIXA;FIXO;.xlsx", colunasCaixaFixo)
+                    + templateBanco(7, "BB Movimento", "Extrato;BB;Movimento;.xlsx", colunasBBMovimento)
+                    + templateBanco(9, "Bradesco", "Extrato;Bradesco;.xlsx", colunasBradesco)
+                    + templateBanco(10, "CEF", "Extrato;Caixa;Federal;.xlsx", colunasCEF)
+                    + templateBanco(11, "Santander", "SANTANDER;.xlsx", colunasSantander)
+                    + templateBanco(2078, "BB Convenio", "Extrato;BB;Convenio;.xlsx", colunasBBConvenio)
+                    + templateBanco(2138, "Servicoop", "SERVICOOP;.xlsx", colunasServicoop)
+            );
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro na aplicação: " + e);
+            System.exit(0);
+        }
+    }
+
+    public static String templateBanco(int contaBanco, String nome, String filtroArquivo, Map<String, String> colunas) {
+        return principal(
+                mes,
+                ano,
+                pastaEmpresa,
+                pastaAnual,
+                pastaMensal,
+                "Template " + nome + " (" + contaBanco + ")",
+                "EcofetalBanco" + contaBanco,
+                filtroArquivo,
+                colunas
+        ) + "\n\n";
+    }
+
+    public static String principal(int mes, int ano, String pastaEmpresa, String pastaAnual, String pastaMensal, String banco, String idTemplate, String filtroArquivo, Map<String, String> colunas) {
+        try {
+            Importation importation = new Importation(Importation.TIPO_EXCEL);
+            importation.setIdTemplateConfig(idTemplate);
+            importation.getExcelCols().putAll(colunas);
+            importation.setNome(banco);
+
+            ControleTemplates controle = new ControleTemplates(mes, ano);
+            controle.setPastaEscMensal(pastaEmpresa);
+            controle.setPasta(pastaAnual, pastaMensal);
+
+            Map<String, Executavel> execs = new LinkedHashMap<>();
+            execs.put("Procurar arquivo " + banco, controle.new defineArquivoNaImportacao(filtroArquivo, importation));
+            execs.put("Criar Template", controle.new converterArquivoParaTemplate(importation));
+
+            return AppRobo.rodarExecutaveis(nomeApp, execs);
+        } catch (Exception e) {
+            return "Ocorreu um erro no Java: " + e;
+        }
+    }
+
+}
--- test/teste/teste.java
+++ test/teste/teste.java
@@ -1,33 +1,33 @@
 package teste;
 
-import importacaoExcelPadrao.importacaoExcelPadrao;
+import importacaoExcelPadrao.EcofetalImportation;
 import java.util.Map;
 import java.util.HashMap;
 
 public class teste {
 
     public static void main(String[] args) {
-        int mes = 10;
+        int mes = 11;
         int ano = 2020;
         String pastaEmpresa = "Ecofetal  Serviço de Auxílio Diagnóstico e Terapia Ltda";
         String pastaAnual = "Movimentos";
         String pastaMensal = "Bancos";
-        String banco = "Template Caixa Fixo (5)";
-        String idTemplate = "EcofetalBanco5";
-        String filtroArquivo = "CAIXA;FIXO;.xlsx";
+        
+        String banco = "Bradesco (9)";
+        String idTemplate = "EcofetalBanco9";
+        String filtroArquivo = "Extrato;Bradesco;.xlsx";
         
         Map<String, String> colunas = new HashMap<>();
-        colunas.put("data", "A");
+        colunas.put("data", "B");
         //colunas.put("documento", "");
-        colunas.put("pretexto", "B");
-        colunas.put("historico", "F");
-        colunas.put("entrada", "G");
-        colunas.put("saida", "-H");
-        //colunas.put("valor", "");
+        //colunas.put("pretexto", "B");
+        colunas.put("historico", "I;C");
+        //colunas.put("entrada", "G");
+        //colunas.put("saida", "-H");
+        colunas.put("valor", "F");
 
 
-        System.out.println(
-                importacaoExcelPadrao.principal(
+        System.out.println(EcofetalImportation.principal(
                         mes,
                         ano,
                         pastaEmpresa,
